c8bdd51d1aea5c485f0b6e40cc4cd2b15729968d
phumulani thusi <thusiphumulani1@gmail.com>
2024-10-29 10:17:56 +0200

    complete work

---
+++ classes.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self,name,age,gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+    
+    def my_self(self):
+        return f'Hi my name is {self.name}, I am {self.age} years old and I am a {self.gender}'
+
+person = Person('Phumulani',10,'male')
+print(person.my_self())
