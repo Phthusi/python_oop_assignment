7d27ee30b17f937f0ea5e990be1d3144e9406553
phumulani thusi <thusiphumulani1@gmail.com>
2024-11-18 14:05:17 +0200

    I used encapsulation, inheritance and polymorphism

--- classes.py
+++ classes.py
@@ -1,11 +1,44 @@
-class Person:
-    def __init__(self,name,age,gender):
+class Animal:
+    def __init__(self, name,body_covering):
         self.name = name
-        self.age = age
-        self.gender = gender
+        self.body_covering = body_covering
     
-    def my_self(self):
-        return f'Hi my name is {self.name}, I am {self.age} years old and I am a {self.gender}'
+    def move_around(self,movement):
+        print(f'{self.name} is {movement}')
+    
+    def make_sound(self, sound):
+        return sound
 
-person = Person('Phumulani',10,'male')
-print(person.my_self())
+
+class Human(Animal):
+    def __init__(self, name,secret):
+        super().__init__(name,'skin')
+        self.__secret = secret
+    
+    def tell_my_secret(self):
+        print(self.__secret)
+
+    def make_sound(self):
+        print(super().make_sound(self.name+' is talking'))
+
+    def move_around(self, movement):
+        return super().move_around(movement)
+
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name, 'fur')
+
+    def make_sound(self):
+        print(super().make_sound(self.name+' says woof woof'))
+
+    def wag_tail(self):
+        print(f"{self.name} is wagging its tail happily!")
+
+person = Human('dave','xyz')
+barky = Dog('barky')
+
+#person.__secret this doesn't work because this attribute is private
+person.tell_my_secret()
+
+for animal in [person,barky]:
+    animal.make_sound()
